cae28cf4f1959dcf026d032aad8e7d62b3b0d36f
NerdXD <kody@nerdxd.com>
2026-07-12 19:26:10 -0400

    Change donation total formatting to two decimal places

--- js/overlay.js
+++ js/overlay.js
@@ -213,8 +213,8 @@
         return new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: currency || "USD",
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 0
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
         }).format(total);
     }
 
